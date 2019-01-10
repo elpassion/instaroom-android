b85895bef75365eba02784da181c9255e83162ee
Paweł Gajda <pawegio@gmail.com>
2019-01-10 15:23:31 +0100

    Change api url

--- app/src/main/java/pl/elpassion/instaroom/api/RetrofitInstaRoomApi.kt
+++ app/src/main/java/pl/elpassion/instaroom/api/RetrofitInstaRoomApi.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 object RetrofitInstaRoomApi : InstaRoomApi {
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://instaroom.app.elpassion.com")
+        .baseUrl("http://pacific-lowlands-76710.herokuapp.com")
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
