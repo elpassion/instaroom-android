3a09d407b22fe2499743f07c221f4c4bb3ba5c6c
bartoszwiercinski <milken93@gmail.com>
2019-01-31 15:41:49 +0100

    fix creating appViewModel in startFragment

--- app/src/main/java/pl/elpassion/instaroom/start/StartFragment.kt
+++ app/src/main/java/pl/elpassion/instaroom/start/StartFragment.kt
@@ -6,16 +6,16 @@ import androidx.navigation.fragment.findNavController
 import com.shopify.livedataktx.first
 import com.shopify.livedataktx.nonNull
 import com.shopify.livedataktx.observe
+import org.koin.android.viewmodel.ext.android.getSharedViewModel
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 import pl.elpassion.instaroom.AppViewModel
 import pl.elpassion.instaroom.R
 
 class StartFragment : Fragment() {
 
-    private val model by sharedViewModel<AppViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        model.toString() // TODO: do anything to create model -> what to do?
+        getSharedViewModel<AppViewModel>()
     }
 }
