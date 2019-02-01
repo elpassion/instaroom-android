b7702272012c582d874075cdf24b65bba881cfd9
bartoszwiercinski <milken93@gmail.com>
2019-02-01 15:07:28 +0100

    add summary test for initialization

--- app/src/test/java/pl/elpassion/instaroom/summary/SummaryModelTest.kt
+++ app/src/test/java/pl/elpassion/instaroom/summary/SummaryModelTest.kt
@@ -2,15 +2,17 @@ package pl.elpassion.instaroom.summary
 
 import androidx.lifecycle.MutableLiveData
 import com.jakewharton.rxrelay2.PublishRelay
+import com.jraska.livedata.test
 import io.kotlintest.IsolationMode
 import io.kotlintest.specs.FreeSpec
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 import org.junit.Assert.*
+import pl.elpassion.instaroom.booking.runBookingFlow
+import pl.elpassion.instaroom.kalendar.Event
 import pl.elpassion.instaroom.util.clearTaskExecutorDelegate
 import pl.elpassion.instaroom.util.executeTasksInstantly
+import java.time.ZonedDateTime
+import java.util.concurrent.TimeUnit
 import kotlin.coroutines.CoroutineContext
 
 class SummaryModelTest : FreeSpec(), CoroutineScope {
@@ -26,9 +28,25 @@ class SummaryModelTest : FreeSpec(), CoroutineScope {
     private val actionS = PublishRelay.create<SummaryAction>()
     private val state = MutableLiveData<SummaryState>()
 
+    private val event = Event(
+        "",
+        "link",
+        "Test event",
+        ZonedDateTime.now().toString(),
+        ZonedDateTime.now().plusHours(1).toString()
+    )
+
+    private val initialState = SummaryState
+
     init {
         executeTasksInstantly()
+        launch { runSummaryFlow(actionS, state, event) }
 
+        val testObserver = state.test()
+
+        "should initialize with expected values" {
+            testObserver.awaitValue(10, TimeUnit.MILLISECONDS).assertValue(initialState)
+        }
 
         job.cancel()
         clearTaskExecutorDelegate()
