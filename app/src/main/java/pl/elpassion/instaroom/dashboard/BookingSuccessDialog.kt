d4d96fe03dff7a94e1333f1c456526fc4bbb7c52
bartoszwiercinski <milken93@gmail.com>
2019-02-01 14:34:11 +0100

    create bookign success dialog

---
+++ app/src/main/java/pl/elpassion/instaroom/dashboard/BookingSuccessDialog.kt
@@ -0,0 +1,32 @@
+package pl.elpassion.instaroom.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import org.koin.android.viewmodel.ext.android.sharedViewModel
+import pl.elpassion.instaroom.AppViewModel
+import pl.elpassion.instaroom.R
+import pl.elpassion.instaroom.TimePickerDialogFragment
+import pl.elpassion.instaroom.kalendar.Event
+
+class BookingSuccessDialog : DialogFragment() {
+
+    private val model by sharedViewModel<AppViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.booking_success_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
